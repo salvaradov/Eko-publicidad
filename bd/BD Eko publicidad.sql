9870fa5295a6d1b1f2dd71b23b80e7baa7b4989e
salvaradov <sebasalvaradov@gmail.com>
2023-11-28 14:54:55 -0600

    cambios a base de datos

--- bd/BD Eko publicidad.sql
+++ bd/BD Eko publicidad.sql
@@ -37,6 +37,20 @@ CREATE TABLE factura(
 	FOREIGN KEY(idProducto) REFERENCES producto(idProducto)
 );
 
+CREATE TABLE usuario(
+	idUsuario INT AUTO_INCREMENT PRIMARY KEY  NOT NULL,
+	idRol INT NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+	correo VARCHAR(50) NOT NULL,
+	contrasena VARCHAR(255) NOT NULL,
+	FOREIGN KEY(idRol) REFERENCES rol(idRol)
+);
+
+CREATE TABLE rol(
+	idRol INT PRIMARY KEY NOT NULL,
+	nombre VARCHAR(20) NOT NULL
+);
+
 
 INSERT INTO producto(idProducto, nombre, descripcion, precio, imagen)
 VALUES(1, 'PROCESADOR AMD RYZEN', 'PROCESADOR AMD RYZEN 3 4100-AM4', 42000.00, 'https://www.intelec.co.cr/image/cache/catalog/catalogo/CPU/100-100000510BOX-800x800.jpg.webp'),
@@ -45,7 +59,15 @@ VALUES(1, 'PROCESADOR AMD RYZEN', 'PROCESADOR AMD RYZEN 3 4100-AM4', 42000.00, '
 (4, 'MONITOR LED CURVO', 'MONITOR LED 23.5" SAMSUNG CF390 CURVO 4MS-60HZ-1920X1080', 79900, 'https://www.intelec.co.cr/image/cache/catalog/catalogo/LC24F390FHLXZP-800x800w.jpg.webp'),
 (5, 'DELL INSPIRON 3520', 'DELL INSPIRON 3520 256GB-SSD-15.6"-W11', 245000.00, 'https://www.intelec.co.cr/image/cache/catalog/catalogo/Portatiles/D5N53-800x800w.jpg.webp');
 
+INSERT INTO rol(idRol, nombre)
+VALUES(1,'Administrador'),
+(2,'Usuario');
+
+INSERT INTO usuario(idRol, nombre, correo, contrasena)
+VALUES(1,'juan', 'salvaradov123@gmail.com', '123'),
+(2, 'camila', 'camila123@gmail.com', '456');
 
 SELECT * FROM producto;
 
+SELECT * FROM usuario;
 
--- producto.php
+++ producto.php
@@ -1,45 +1,31 @@
+<?php
+    session_start();
+    include("header.html");
+    include("conexionDB.php");
+?>
+
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Producto</title>
+    <title>Productos</title>
+
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
+
+
 </head>
 <body>
-    <h1>PRODUCTO</h1>
-    <nav class="navegación">
-        <a class="navegación_enlace" href="cliente.php">Cliente</a>
-        <a class="navegación_enlace" href="facturacion.php">Facturación</a>
-        <a class="navegación_enlace" href="ingresar.php">Ingresar</a>
-    </nav>
-    <p>Ingrese un producto</p>
-    <p>ID producto</p>
-    <p><input type="text" name="Id producto"  value="Id producto" size="20" maxlength="20"></p>
-    <p>Nombre de producto</p>
-    <p><input type="text" name="ingrese el producto" value="nombre de producto" size="20" maxlength="20"></p>
-    <p>Descripción del producto</p>
-    <p><input type="text" name="ingrese la descripción del producto" value="descripción" size="20" maxlength="20"></p>
-    <p>Precio por unidad</p>
-    <p><input type="number" name="ingrese el precio por unidad" value="precio por unidad" size="20" maxlength="20"></p>
-    <p>
-        <input type="submit" value="Ingresar">
-    </p>
-    <p>
-        <input type="reset" value="Regresar">
-    </p>
-    <p>
-        <input type="reset" value="Modificar">
-    </p>
-    <p>
-        <input type="submit" value="Eliminar">
-    </p>    
+   
+
+
+
 
 </body>
 </html>
-
     
